c7431a913bd3a76fdb133adec9170af65d4e67a8
Carl Lubojanski <carl.lubojanski@outlook.com>
2026-09-09 21:16:14 +0200

    setup repo

---
+++ src/learning_actions/__init__.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    print("Hello from learning-actions!")
+
+def add(a, b):
+    return a + b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
